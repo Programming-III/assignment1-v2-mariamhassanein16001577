a1a3108483687318765c482da80118ba5def4063
mariamhassanein16001577 <mariam.hassanein@student.giu-uni.de>
2025-11-11 09:55:46 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,21 +1,18 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
 #include "Person.h"
 #include <string>
-#using namespace std;
-//#write student class here
 
+using namespace std;
 
-
-
-
-
-
-
-
-
-
-
-
+class Student {
+private:
+    int yearLevel=0;
+    string major;
+    void display() const {
+        cout << yearLevel << endl;
+        cout << major << endl;
+    }
+    ~ Student() {}
+};
 #endif
