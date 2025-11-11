a55485c118f53917052122daa555ece3abafd730
mariamhassanein16001577 <mariam.hassanein@student.giu-uni.de>
2025-11-11 09:54:43 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,19 +4,30 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents=0;
+    Student *students = nullptr;
+    int currentStudents=0;
+    void addStudent(const Student& s) {
+        s =
+    }
+    static void getMajor(){
+        Student.major = getMajor;
+    }
+    static void getYearLevel() {
+        Student.yearLevel =  getYearLevel;
+    }
+    void displayCourseInfo() const {
+        cout << courseCode << endl;
+        cout << courseName << endl;
+        cout << currentStudents << endl;
+        cout << getMajor << endl;
+        cout << getYearLevel << endl;
+    }
+    ~ Course();
+};
 #endif
